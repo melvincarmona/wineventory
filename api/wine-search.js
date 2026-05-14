a6489e3fb00997824534e4e0077905b5975dc5cb
mcarmona <melvin.gian.carmona@gmail.com>
2026-05-14 17:32:35 +0200

    Modellname korrigiert

--- api/wine-search.js
+++ api/wine-search.js
@@ -12,7 +12,7 @@ module.exports = async function handler(req, res) {
     });
 
     const message = await client.messages.create({
-      model: "claude-opus-4-5",
+      model: "claude-haiku-4-5-20251001",
       max_tokens: 1024,
       messages: [
         {
